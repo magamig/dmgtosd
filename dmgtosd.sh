b7f3561f9dfcc54ddd29d09f879c0418548b4c72
magamig <magamig@gmail.com>
2015-01-18 19:56:02 +0000

    Create dmgtosd.sh

---
+++ dmgtosd.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+#    DMG to SD restore [OSX] 
+#    
+#    Copyright (C) 2015 magamig
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ `id -u` -ne 0 ]
+then
+  echo "Please start this script with root privileges!"
+  echo "Try again with sudo."
+  exit 0
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+	echo "DMG to SD card - Restore"
+	echo ""
+
+	echo "Path to DISK: (example: /dev/disk2)"
+	read DISK
+	echo "Path to DMG file: (example: /Users/Bob/Desktop/foo.dmg)"
+	read DMG
+
+	echo "Copying "$DMG" to "$DISK
+	mkdir /.dmgtosd
+	hdiutil convert $DMG -format UDTO -o /.dmgtosd/bar.img
+	mv /.dmgtosd/bar.img.cdr /.dmgtosd/bar.img
+	clear
+	diskutil umountDisk $DISK
+	sleep 1
+	clear
+	pv -p -e -r -t -r /.dmgtosd/bar.img | dd of=$DISK
+	sleep 1
+	rm -r /.dmgtosd
+	echo "Success. Everythind is complete!"
+	exit 0
+else
+	echo "This script is designed for OS X only."
+	exit 0
+fi
